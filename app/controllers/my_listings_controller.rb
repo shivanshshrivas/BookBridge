04a62d1239f4c58efdcfd8538dc5e660d7b390b9
Shivansh Shrivas <shrivasshivansh@gmail.com>
2025-07-28 06:57:25 +0000

    lint fix

--- app/controllers/my_listings_controller.rb
+++ app/controllers/my_listings_controller.rb
@@ -12,11 +12,11 @@ class MyListingsController < ApplicationController
   def create
     @listing = current_user.listings.build(listing_params)
     @listing.university = current_user.university
-    @listing.price = params[:listing][:listing_type] == 'sell' ? params[:listing][:price] : nil
+    @listing.price = (params[:listing][:listing_type] == "sell") ? params[:listing][:price] : nil
     if @listing.save
-      redirect_to my_listings_path, notice: 'Listing created successfully.'
+      redirect_to my_listings_path, notice: "Listing created successfully."
     else
-      render :new,  status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -30,6 +30,7 @@ class MyListingsController < ApplicationController
   end
 
   private
+
   def listing_params
     params.expect(listing: [:isbn, :title, :authors, :edition, :course_number, :description, :listing_type, :price, images: []])
   end
