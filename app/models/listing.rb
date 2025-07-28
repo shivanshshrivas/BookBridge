234ab537fc864b091d15d002f1fb3cf9d7127d76
Shivansh Shrivas <shrivasshivansh@gmail.com>
2025-07-28 13:18:47 +0000

    added ransack gem for querying

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,8 @@ class Listing < ApplicationRecord
   has_many_attached :images
 
   validates :title, :authors, :description, :listing_type, :university, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[title authors listing_type isbn course_number university updated_at]
+  end
 end
