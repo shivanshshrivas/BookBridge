14db3bb70b4c499e352e83dcaa2e04aa4cb5e16e
Shivansh Shrivas <shrivasshivansh@gmail.com>
2025-07-30 22:19:02 +0000

    Added error handling in the form and no results for a search function

--- app/frontend/controllers/search_controller.js
+++ app/frontend/controllers/search_controller.js
@@ -3,17 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search"
 export default class extends Controller {
-    static targets = ["optionA", "optionB", "optionC", "optionD", "input"]
+    static targets = ["optionA", "optionB", "optionC", "optionD", "searchInput"]
   connect() {
     console.log("Search controller connected");
     this.updateVisibility();
   }
 
   updateVisibility() {
-    const selectedOption = this.inputTarget.value;
+    const selectedOption = this.searchInputTarget.value;
     this.optionATarget.classList.toggle("hidden", selectedOption !== "title");
+    this.optionATarget.disabled = selectedOption !== "title";
     this.optionBTarget.classList.toggle("hidden", selectedOption !== "authors");
+    this.optionBTarget.disabled = selectedOption !== "authors";
     this.optionCTarget.classList.toggle("hidden", selectedOption !== "isbn");
+    this.optionCTarget.disabled = selectedOption !== "isbn";
     this.optionDTarget.classList.toggle("hidden", selectedOption !== "course_number");
+    this.optionDTarget.disabled = selectedOption !== "course_number";
+    console.log(`Selected option: ${selectedOption}`);
+    
   }
 }
