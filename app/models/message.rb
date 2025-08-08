6246a3d95e9c885f12e53aaf9907ff8f8b1778c0
Shivansh Shrivas <shrivasshivansh@gmail.com>
2025-08-08 19:09:20 +0000

    customized models

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,22 @@
 class Message < ApplicationRecord
   belongs_to :listing
+  belongs_to :sender, class_name: "User"
+  belongs_to :receiver, class_name: "User"
   belongs_to :messageable, polymorphic: true
+
+  validates :participants_are_distinct
+
+  scope :between, ->(u1, u2, listing_id) {
+    where(listing_id:, sender_id: [u1, u2], receiver_id: [u1, u2]).order(:created_at)
+  }
+
+  def other_party_for(user) = (user.id == sender_id) ? receiver : sender
+
+  private
+
+  def participants_are_distinct
+    if sender_id == receiver_id
+      errors.add(:base, "Sender and receiver must be distinct users")
+    end
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,18 @@
 class Transaction < ApplicationRecord
   belongs_to :listing
+  belongs_to :lender, class_name: "User"
+  belongs_to :borrower, class_name: "User"
+  has_one :message, as: :messageable, dependent: :nullify
+
+  enum :status, {pending: 0, in_progress: 1, completed: 2, cancelled: 3}
+
+  validate :parties_are_distinct
+
+  private
+
+  def parties_are_distinct
+    if lender_id == borrower_id
+      errors.add(:base, "Lender and borrower must be distinct users")
+    end
+  end
 end
